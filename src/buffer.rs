265ed53e296a4f7473face349d83283574c4a6e6
D-McInnes8 <96274982+D-McInnes8@users.noreply.github.com>
2024-02-18 16:38:30 +1000

    Refactored text buffer code and started delete functionality

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,18 +1,11 @@
-use log::warn;
+use log::{info, warn};
 
 #[derive(Debug)]
 pub struct TextBuffer {
-    original: Vec<char>,
-    add: Vec<char>,
-    table: Vec<Node>,
+    original: String,
+    add: String,
+    table: Vec<Span>,
     lines: Vec<usize>,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Node {
-    buffer: BufferType,
-    start: usize,
-    length: usize,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -21,87 +14,201 @@ pub enum BufferType {
     Add,
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct Span {
+    buffer: BufferType,
+    start: usize,
+    end: usize,
+    len: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DocumentPiece {
+    index: usize,
+    span: Span,
+    pos: DocumentPosition,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DocumentPosition {
+    start: u32,
+    end: u32,
+}
+
+impl Span {
+    pub fn new(buffer: BufferType, start: usize, len: usize) -> Span {
+        Span {
+            buffer,
+            start,
+            end: start + len,
+            len,
+        }
+    }
+}
+
 impl TextBuffer {
     pub fn new(text: Option<&str>) -> TextBuffer {
         let mut buffer = TextBuffer {
-            original: text.unwrap_or("").chars().collect(),
-            add: Vec::with_capacity(1000),
+            original: text.unwrap_or("").to_owned(),
+            add: String::new(),
             table: Vec::with_capacity(500),
             lines: Vec::with_capacity(10),
         };
 
-        buffer.table.push(Node {
-            buffer: BufferType::Original,
-            start: 0,
-            length: text.map_or(0, |x| x.len()),
-        });
+        buffer.table.push(Span::new(
+            BufferType::Original,
+            0,
+            text.map_or(0, |x| x.len()),
+        ));
 
         buffer
     }
 
     pub fn append(&mut self, text: &str) {
-        let pos = self.add.len();
-        self.add.append(&mut text.chars().collect::<Vec<char>>());
-        self.table.push(Node {
-            buffer: BufferType::Add,
-            start: pos,
-            length: text.len(),
-        });
+        let pos = self.add_to_buffer(text);
+        self.table.push(Span::new(BufferType::Add, pos, text.len()));
+    }
+
+    pub fn prepend(&mut self, text: &str) {
+        let pos = self.add_to_buffer(text);
+        //self.table.insert(0, Node::add(pos, text.len()));
+        self.table
+            .insert(0, Span::new(BufferType::Add, pos, text.len()));
     }
 
     fn add_to_buffer(&mut self, text: &str) -> usize {
         let pos = self.add.len();
-        self.add.append(&mut text.chars().collect::<Vec<char>>());
+        self.add += text;
         pos
     }
 
     pub fn insert(&mut self, pos: usize, text: &str) {
+        info!("Inserting {} at position {}", text, pos);
+        //eprintln!();
+        //eprintln!("Inserting {} at position {}", text, pos);
+
+        // position is at the start
+        if pos == 0 {
+            self.prepend(text);
+            return;
+        }
+
+        // position is at the end
+        if pos == self.doc_len() {
+            self.append(text);
+            return;
+        }
+
+        // position is in the middle
         if let Some((piece, piece_index, pos_in_document)) = &self.get_piece_at_position(pos) {
-            eprintln!("pos: {}, pos in document: {}", pos, pos_in_document);
-            let diff = pos - pos_in_document;
+            let pos_in_add_buffer = self.add_to_buffer(text);
 
-            let p1_start = piece.start;
-            let p1_length = pos_in_document + pos;
+            /*eprintln!("{:?}", self);
+            eprintln!(
+                "piece: {:?}, piece index: {}, doc position: {}",
+                piece, piece_index, pos_in_document
+            );
+            eprintln!(
+                "p3 length: {} - {} - {}",
+                piece.length, pos, pos_in_document
+            );*/
 
-            let p2_start = 0;
-            let p2_length = text.len();
+            let piece1 = Span::new(piece.buffer, piece.start, pos - pos_in_document); //pos_in_document + pos);
+            let piece2 = Span::new(BufferType::Add, pos_in_add_buffer, text.len());
 
-            let p3_start = p1_start + p1_length;
-            let p3_length = piece.length - diff;
+            /*eprintln!();
+            eprintln!("piece1: {:?}", piece1);
+            eprintln!("piece2: {:?}", piece2);
+            eprintln!("{} - ({} + {})", piece.length, piece1.start, piece1.length);
+            eprintln!();*/
+            let piece3 = Span::new(
+                piece.buffer,
+                piece1.start + piece1.len,
+                piece.len - (piece1.start + piece1.len),
+                //piece.length - pos - pos_in_document,
+            );
 
-            let append_pos = self.add.len();
-            let mut chars = text.chars().collect::<Vec<char>>();
-            //self.add.append(&mut vec![]);
-            //self.add.append(&mut text.chars().collect::<Vec<char>>());
-
-            let append_pos = self.add_to_buffer(text);
-
-            let p1 = Node {
-                buffer: piece.buffer.to_owned(),
-                start: p1_start,
-                length: p1_length,
-            };
-            let p2 = Node {
-                buffer: BufferType::Add,
-                start: append_pos,
-                length: p2_length,
-            };
-            let p3 = Node {
-                buffer: piece.buffer.to_owned(),
-                start: p3_start,
-                length: p3_length,
-            };
-
-            self.table[*piece_index].start = p1_start;
-            self.table[*piece_index].length = p1_length;
-            self.table.insert(piece_index + 1, p3);
-            self.table.insert(piece_index + 1, p2);
+            self.table[*piece_index] = piece1;
+            self.table.insert(piece_index + 1, piece3);
+            self.table.insert(piece_index + 1, piece2);
         } else {
             warn!("Position {} is too large", pos);
         }
     }
 
-    pub fn delete(&mut self, pos: usize, length: usize) {}
+    pub fn delete(&mut self, start: usize, end: usize) {
+        let len = end - start;
+        eprintln!("start: {}, end: {}", start, end);
+        eprintln!("len: {}", len);
+        eprintln!("buffer: {:?}", self);
+
+        // start and end are in the same piece:
+        //     1. split the piece into two new pieces.
+        // start and end are in different pieces:
+        //     1. set new length for start piece.
+        //     2. set new start for end piece.
+        //     3. remove any pieces between these two pieces.
+        let p1 = self.get_piece_at_position(start);
+        let p2 = self.get_piece_at_position(end);
+
+        eprintln!(
+            "p1: {:?}. text: {}",
+            p1,
+            self.get_text_for_piece(p1.unwrap().1)
+        );
+        eprintln!(
+            "p2: {:?}. text: {}",
+            p2,
+            self.get_text_for_piece(p2.unwrap().1)
+        );
+
+        match (p1, p2) {
+            (Some((p1, i1, d1)), Some((p2, i2, d2))) if i1 == i2 => {
+                eprintln!("delete from single piece.");
+                let start_relative = start - d1;
+                let end_relative = start + len;
+                self.split_piece(i1, start_relative, end_relative);
+            }
+            (Some((p1, i1, d1)), Some((p2, i2, d2))) => {
+                eprintln!("delete from multiple pieces.");
+                self.delete_multiple(i1, i2);
+            }
+            (Some((a, b, c)), None) => {}
+            _ => {
+                eprintln!("none");
+            }
+        };
+    }
+
+    fn split_piece(&mut self, index: usize, start: usize, end: usize) {
+        // buffer   start length
+        // original 0     22
+        //
+        // delete 15-20
+        //
+        // buffer   start length func
+        // original 0     15     (ex.start) (start)
+        // original 20    22     (ex.start + end) (ex.length - end)
+        let ex = &self.table[index];
+        let p1 = Span::new(ex.buffer, ex.start, start);
+        let p2 = Span::new(ex.buffer, ex.start + end, ex.len - end);
+
+        self.table[index] = p1;
+        self.table.insert(index + 1, p2);
+    }
+
+    fn delete_multiple(&mut self, i1: usize, i2: usize) {
+        let p1 = &self.table[i1];
+        let p2 = &self.table[i2];
+
+        // Remove and pieces between the two pieces.
+        if i2 - i1 > 1 {
+            for i in i1 + 1..i2 - 1 {
+                eprintln!("index: {}", i);
+                //self.table.remove(i);
+            }
+        }
+    }
 
     pub fn text(&self) -> String {
         let mut text = String::new();
@@ -109,12 +216,12 @@ impl TextBuffer {
         for row in &self.table {
             match row.buffer {
                 BufferType::Original => {
-                    let c = &self.original[row.start..(row.start + row.length)];
-                    text += c.iter().collect::<String>().as_str();
+                    let c = &self.original[row.start..(row.start + row.len)];
+                    text += c;
                 }
                 BufferType::Add => {
-                    let c = &self.add[row.start..(row.start + row.length)];
-                    text += c.iter().collect::<String>().as_str();
+                    let c = &self.add[row.start..(row.start + row.len)];
+                    text += c;
                 }
             }
         }
@@ -122,17 +229,49 @@ impl TextBuffer {
         text
     }
 
-    fn get_piece_at_position(&self, pos: usize) -> Option<(Node, usize, usize)> {
+    fn get_piece_at_position(&self, pos: usize) -> Option<(Span, usize, usize)> {
         let mut current_pos = 0;
 
         for (i, piece) in self.table.iter().enumerate() {
-            current_pos += piece.length;
-            if current_pos >= pos {
-                return Some((piece.clone(), i, current_pos - piece.length));
+            /*eprintln!(
+                "index: {}, piece: {:?}. text: '{}'",
+                i,
+                piece,
+                self.get_text_for_piece(i)
+            );*/
+            //current_pos += piece.length;
+            eprintln!("current pos: {}", current_pos);
+            if current_pos + piece.len >= pos {
+                //eprintln!("Returning piece");
+                eprintln!("piece: {:?}. text: {}", piece, self.get_text_for_piece(i));
+                eprintln!("{} >= {}", current_pos, pos);
+                return Some((piece.clone(), i, current_pos));
             }
+
+            current_pos += piece.len;
         }
 
+        eprintln!(
+            "Invalid position. Pos: {}, Current pos: {}",
+            pos, current_pos
+        );
         None
+    }
+
+    fn doc_len(&self) -> usize {
+        let mut current_pos = 0;
+        for (i, piece) in self.table.iter().enumerate() {
+            current_pos += piece.len;
+        }
+        current_pos
+    }
+
+    fn get_text_for_piece(&self, pos: usize) -> &str {
+        let p = &self.table[pos];
+        match p.buffer {
+            BufferType::Add => &self.add[p.start..(p.start + p.len)],
+            BufferType::Original => &self.original[p.start..(p.start + p.len)],
+        }
     }
 }
 
@@ -143,34 +282,33 @@ mod tests {
     #[test]
     fn construct_text() {
         let buffer = TextBuffer {
-            original: vec![
-                'i', 'p', 's', 'u', 'm', ' ', 's', 'i', 't', ' ', 'a', 'm', 'e', 't',
-            ],
+            original: String::from("ipsum sit amet"),
+            add: String::from("Lorem deletedtext dolor"),
             lines: vec![],
-            add: vec![
-                'L', 'o', 'r', 'e', 'm', ' ', 'd', 'e', 'l', 'e', 't', 'e', 'd', 't', 'e', 'x',
-                't', ' ', 'd', 'o', 'l', 'o', 'r',
-            ],
             table: vec![
-                Node {
+                Span {
                     buffer: BufferType::Add,
                     start: 0,
-                    length: 6,
+                    len: 6,
+                    end: 6,
                 },
-                Node {
+                Span {
                     buffer: BufferType::Original,
                     start: 0,
-                    length: 5,
+                    len: 5,
+                    end: 5,
                 },
-                Node {
+                Span {
                     buffer: BufferType::Add,
                     start: 17,
-                    length: 6,
+                    len: 6,
+                    end: 23,
                 },
-                Node {
+                Span {
                     buffer: BufferType::Original,
                     start: 5,
-                    length: 9,
+                    len: 9,
+                    end: 14,
                 },
             ],
         };
@@ -202,5 +340,15 @@ mod tests {
     }
 
     #[test]
-    fn delete() {}
+    fn delete() {
+        let mut buffer = TextBuffer::new(Some("ipsum sit amet"));
+        buffer.insert(0, "Lorem ");
+        buffer.insert(11, "deletedtext");
+        buffer.insert(11, " dolor");
+        buffer.delete(17, 28);
+
+        let expected = "Lorem ipsum dolor sit amet";
+        let actual = buffer.text();
+        assert_eq!(expected, actual);
+    }
 }
